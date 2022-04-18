1d4d8a305990ec59e32e57254b730280abf42f31
codelrond <codelrond@protonmail.com>
2022-04-18 19:15:27 +0200

    adding helpers function extend from M to H and from H to M

--- src/myf.jl
+++ src/myf.jl
@@ -1,9 +1,43 @@
+@enum TIPO_CONSUMO TIPO_CONSUMO_IGUAL=1 TIPO_CONSUMO_NOCHE
+
+function extend_from_H_to_M(x::AbstractArray{Float64, 1})
+    repeat(x, 12)
+end
+
+function extend_from_M_to_H(x::AbstractArray{Float64, 1})
+    repeat(x', 1, 24)
+end
+
 function get_auxM(perfil_consumo_anual::Int64, perfil_consumo_diario::Int64, consumo_anual::Float64)
     zeros(12)
 end
 
-function getauxH(perfil_consumo_diario::Int64)
+function get_equitativo_H()
     zeros(24)
+end
+
+function get_factor_noche()
+    zeros(24)
+end
+
+function get_factor_tarde()
+    zeros(24)
+end
+
+function get_aux_H(perfil_consumo_diario::Int64)
+    equitativo = get_equitativo_H()
+    factor_noche = get_factor_noche()
+    factor_tarde = get_factor_tarde()
+
+    map(zip(equitativo, factor_noche, factor_tarde)) do (e, n, t)
+        if perfil_consumo_diario == TIPO_CONSUMO_IGUAL
+            e
+        elseif perfil_consumo_diario == TIPO_CONSUMO_NOCHE
+            n*e
+        else 
+            t*e
+        end
+    end
 end
 
 #function MH
@@ -11,11 +45,11 @@ end
 #end
 
 function get_perfil_personalizado(perfil_consumo_anual::Int64, perfil_consumo_diario::Int64, consumo_anual::Float64)
-    auxH = getauxH(perfil_consumo_diario)
+    auxH = get_aux_H(perfil_consumo_diario)
     auxM = get_auxM(perfil_consumo_anual, perfil_consumo_diario, consumo_anual)
 
-    auxH = repeat(auxH, 12)
-    auxM = repeat(auxM', 1, 24)
+    auxH = extend_from_H_to_M(auxH)
+    auxM = extend_from_M_to_H(auxM)
     
     map(zip(auxH, auxM)) do (h, m)
         1000.0h*m
