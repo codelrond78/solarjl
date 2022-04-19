95813dde0f3fe36eaa5e732025f18447e8e9b119
codelrond <codelrond@protonmail.com>
2022-04-19 17:54:53 +0200

    real request to radiation and parse to structure

--- src/radiation.jl
+++ src/radiation.jl
@@ -7,17 +7,25 @@ using HTTP
 using JSON3
 using StructTypes
 
+struct Output
+    daily_profile::Vector{Dict}
+end
 struct Radiation
-    a::Float64 
+    inputs::Dict
+    outputs::Output
+    meta::Dict
 end
 
 StructTypes.StructType(::Type{Radiation}) = StructTypes.Struct()
 
-function get_radiation(latitude::Float64, longitude::Float64)
-    x = HTTP.request("GET", "http://localhost:3000/posts")
-    body = String(x.body)
-    #JSON.parse(body)
-    JSON3.read(body, Radiation)
+function get_radiation(latitude::Float64, longitude::Float64, angle::Float64, azimut::Float64)
+    month = 0
+    url = "https://re.jrc.ec.europa.eu/api/DRcalc?lat=$latitude&lon=$longitude&raddatabase=PVGIS-SARAH&angle=$angle&aspect=$azimut&outputformat=json&userhorizon=&usehorizon=1&js=1&select_database_daily=PVGIS-SARAH&month=$month&localtime=1&global=1&clearsky=1&dangle=$angle&daspect=$azimut&glob_2axis=1"
+    x = HTTP.request("GET", url)
+    body = String(x.body)    
+    y = JSON3.read(body, Radiation)
+    println(y.outputs.daily_profile)
+    y
 end
 
 end
