161811e308b81b8afadbaeab8a12829a227947fe
codelrond <codelrond@protonmail.com>
2022-04-19 08:39:45 +0200

    added JSON parse to Float64

--- src/radiation.jl
+++ src/radiation.jl
@@ -1,8 +1,18 @@
+using Genie, Genie.Router, Genie.Requests, Genie.Renderer.Json
 using HTTP
+using JSON
 
 function radiation(latitude::Float64, longitude::Float64)
-    HTTP.request("GET", "http://httpbin.org/ip")
+    println("previo request")
+    x = HTTP.request("GET", "http://localhost:3000/posts")
+    println("x")
+    body = String(x.body)
+    println("body")
+    JSON.parse(body)
 end
-#r = HTTP.request("GET", "http://httpbin.org/ip"; verbose=3)
-#println(r.status)
-#println(String(r.body))
+
+println("empezamos")
+response = radiation(0.0, 0.0)
+println("response")
+x = get(response[1], "a", missing)
+println(x, typeof(x))
