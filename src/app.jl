6148ca0761973559d2f20196600d695cb188bc26
codelrond <codelrond@protonmail.com>
2022-04-19 10:02:11 +0200

    added CORS support

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,10 @@
 using Genie, Genie.Router, Genie.Requests, Genie.Renderer.Json
 
+#Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3001"
+Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
+Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST,PUT,DELETE,OPTIONS" 
+Genie.config.cors_allowed_origins = ["*"]
+
 struct myData
     x
 end
