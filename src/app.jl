cbafa08942587c85e9c89e768100bf1be73da990
codelrond <codelrond@protonmail.com>
2022-04-19 11:21:49 +0200

    wip

--- src/app.jl
+++ src/app.jl
@@ -1,6 +1,6 @@
 using Genie, Genie.Router, Genie.Requests, Genie.Renderer.Json
 
-#Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3001"
+Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3001"
 Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
 Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST,PUT,DELETE,OPTIONS" 
 Genie.config.cors_allowed_origins = ["*"]
@@ -12,6 +12,8 @@ end
 route("/jsonpayload", method = POST) do
   @show jsonpayload()
   @show rawpayload()
+
+  println(typeof(jsonpayload()["a"]))
 
   json(myData(5))
 end
