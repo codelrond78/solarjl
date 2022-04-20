e818911c0b084cfa7fa034fb656fbd1d7f467d1a
codelrond <codelrond@protonmail.com>
2022-04-20 17:06:13 +0200

    validating user input and tests

--- src/mytest.jl
+++ src/mytest.jl
@@ -1,6 +1,8 @@
+include("./user_input.jl")
 include("./radiation.jl")
 using Test
 using .radiation
+using .user
 
 @testset "time pos" begin    
     @test time_pos("00:00") == 0
@@ -14,4 +16,9 @@ end;
 
     output = Output([Dict("time" => "00:00", "month" => 1, "G(i)" => 5.3)])
     @test radiation_mx(output; d=2, h=2) == [5.3 0.0; 0.0 0.0]
+end;
+
+@testset "validate user input" begin    
+    datajson = """{"angle": 0.0, "azimut": 0.0}""" 
+    @test_throws MissingException get_user_input(datajson)
 end;
--- src/user_input.jl
+++ src/user_input.jl
@@ -1,19 +1,60 @@
 module user
 
-export UserInput
-export get_user_input
+export get_user_input, UserInput
 
 using JSON3
 using StructTypes
 
 struct UserInput
-   a::Float64 
+   angle::Float64 
+   azimut::Float64 
+   yearconsume::Float64
+   typedayconsume::Int64
+   typeyearconsume::Int64
+   numpanels::Int64
+   bonopercentage::Int64
 end
 
 StructTypes.StructType(::Type{UserInput}) = StructTypes.Struct()
 
 function get_user_input(payload::String)
-    JSON3.read(payload, UserInput)
+    try
+        ui = JSON3.read(payload, UserInput)
+    catch
+        throw(MissingException("Error parsing payload"))
+    end
+    v = validate(ui)
+    if length(v) == 0 
+        return ui
+    else
+        throw(DomainError(ui, join(v, "\n")))
+    end
+end
+
+function validate(input::UserInput)
+    response = []
+    if input.angle < 0.0 || input.angle > 90.0 
+        push!(response, "angle: must be 0.0 <= angle <= 90.0")
+    end
+    if input.azimut < -90.0 || input.angle > 90.0
+        push!(response, "azimut: must be -90.0 <= azimut <= 90.0")
+    end
+    if input.yearconsume < 0
+        push!(response, "yearconsume: must be yearconsume >= 0")
+    end
+    if input.typedayconsume < 1 || input.typedayconsume > 3
+        push!(response, "typedayconsume: must be typedayconsume in [1, 2, 3]")
+    end
+    if input.typeyearconsume < 1 || input.typeyearconsume > 3
+        push!(response, "typeyearconsume: must be typeyearconsume in [1, 2, 3]")
+    end
+    if input.numpanels < 0
+        push!(response, "numpanels: must be numpanels >= 0")
+    end
+    if input.bonopercentage < 0 || input.bonopercentage > 1
+        push!(response, "bonopercentage: must be bonopercentage in [0, 1]")
+    end
+    response
 end
 
 end  
