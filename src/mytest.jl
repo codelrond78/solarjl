e77f8ee40cfb4aacc6556e3b11d71694a860b957
codelrond <codelrond@protonmail.com>
2022-04-20 19:01:57 +0200

    adding detailed validation of user input

--- src/mytest.jl
+++ src/mytest.jl
@@ -4,21 +4,26 @@ using Test
 using .radiation
 using .user
 
-@testset "time pos" begin    
-    @test time_pos("00:00") == 0
-    @test time_pos("01:00") == 1
-    @test time_pos("10:00") == 10
-end;
+function run()
+    @testset "time pos" begin    
+        @test time_pos("00:00") == 0
+        @test time_pos("01:00") == 1
+        @test time_pos("10:00") == 10
+    end;
 
-@testset "radiation mx" begin    
-    output = Output([])
-    @test radiation_mx(output; d=2, h=2) == zeros(2, 2)
+    @testset "radiation mx" begin    
+        output = Output([])
+        @test radiation_mx(output; d=2, h=2) == zeros(2, 2)
 
-    output = Output([Dict("time" => "00:00", "month" => 1, "G(i)" => 5.3)])
-    @test radiation_mx(output; d=2, h=2) == [5.3 0.0; 0.0 0.0]
-end;
+        output = Output([Dict("time" => "00:00", "month" => 1, "G(i)" => 5.3)])
+        @test radiation_mx(output; d=2, h=2) == [5.3 0.0; 0.0 0.0]
+    end;
 
-@testset "validate user input" begin    
-    datajson = """{"angle": 0.0, "azimut": 0.0}""" 
-    @test_throws MissingException get_user_input(datajson)
-end;
+    @testset "validate user input" begin    
+        datajson = """{"angle": 0.0, "azimut": 0.0}""" 
+        @test_throws MissingInput get_user_input(datajson)
+        datajson = """{"angle": 0.0, "azimut": 0.0, "yearconsume": 0.0, "typedayconsume": 0, "typeyearconsume": 0, "numpanels": -1, "bonopercentage": 0}""" 
+        @test_throws DomainErrorInput get_user_input(datajson)
+    end;
+end
+
--- src/user_input.jl
+++ src/user_input.jl
@@ -1,60 +1,81 @@
 module user
 
-export get_user_input, UserInput
+export get_user_input, UserInput, MissingInput, DomainErrorInput
 
 using JSON3
 using StructTypes
+using Exceptions
+
+mutable struct InputErrors <: Exception
+    msg
+end
 
 struct UserInput
-   angle::Float64 
-   azimut::Float64 
-   yearconsume::Float64
-   typedayconsume::Int64
-   typeyearconsume::Int64
-   numpanels::Int64
-   bonopercentage::Int64
+   angle
+   azimut
+   yearconsume
+   typedayconsume
+   typeyearconsume
+   numpanels
+   bonopercentage
 end
 
 StructTypes.StructType(::Type{UserInput}) = StructTypes.Struct()
 
 function get_user_input(payload::String)
-    try
-        ui = JSON3.read(payload, UserInput)
-    catch
-        throw(MissingException("Error parsing payload"))
-    end
-    v = validate(ui)
-    if length(v) == 0 
-        return ui
-    else
-        throw(DomainError(ui, join(v, "\n")))
-    end
+    ui = JSON3.read(payload, UserInput)
+    validate(ui)  
+    ui
 end
 
 function validate(input::UserInput)
     response = []
-    if input.angle < 0.0 || input.angle > 90.0 
+
+    if isnothing(input.angle)
+        push!(response, "angle: missing")
+    elseif input.angle < 0.0 || input.angle > 90.0 
         push!(response, "angle: must be 0.0 <= angle <= 90.0")
     end
-    if input.azimut < -90.0 || input.angle > 90.0
+
+    if isnothing(input.azimut)
+        push!(response, "azimut: missing")
+    elseif input.azimut < -90.0 || input.angle > 90.0
         push!(response, "azimut: must be -90.0 <= azimut <= 90.0")
     end
-    if input.yearconsume < 0
+
+    if isnothing(input.yearconsume)
+        push!(response, "yearconsume: missing")
+    elseif input.yearconsume < 0
         push!(response, "yearconsume: must be yearconsume >= 0")
     end
-    if input.typedayconsume < 1 || input.typedayconsume > 3
+
+    if isnothing(input.typedayconsume)
+        push!(response, "typedayconsume: missing")
+    elseif input.typedayconsume < 1 || input.typedayconsume > 3
         push!(response, "typedayconsume: must be typedayconsume in [1, 2, 3]")
     end
-    if input.typeyearconsume < 1 || input.typeyearconsume > 3
+
+    if isnothing(input.typeyearconsume)
+        push!(response, "typeyearconsume: missing")
+    elseif input.typeyearconsume < 1 || input.typeyearconsume > 3
         push!(response, "typeyearconsume: must be typeyearconsume in [1, 2, 3]")
     end
-    if input.numpanels < 0
+
+    if isnothing(input.numpanels)
+        push!(response, "numpanels: missing")
+    elseif input.numpanels < 0
         push!(response, "numpanels: must be numpanels >= 0")
     end
-    if input.bonopercentage < 0 || input.bonopercentage > 1
+
+    if isnothing(input.bonopercentage)
+        push!(response, "bonopercentage: missing")
+    elseif input.bonopercentage < 0 || input.bonopercentage > 1
         push!(response, "bonopercentage: must be bonopercentage in [0, 1]")
     end
-    response
+
+    if length(response) != 0 
+        throw(InputErrors(join(response, "\n")))
+    end
 end
 
 end  
