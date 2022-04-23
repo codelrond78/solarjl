5c64ecd55318acec276efeca130a29f60e4207e1
codelrond78 <codelrond@protonmail.com>
2022-04-23 18:37:43 +0200

    tested CORS

--- src/app.jl
+++ src/app.jl
@@ -1,26 +1,26 @@
 using Genie, Genie.Router, Genie.Requests, Genie.Renderer.Json
+#=
 include("load_data.jl")
 include("user_input.jl")
 using .user
 include("radiation.jl")
 include("calculate.jl")
+=#
 
-Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3001"
+#Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3001"
 Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
 Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST,PUT,DELETE,OPTIONS" 
 Genie.config.cors_allowed_origins = ["*"]
 
 function main()
-    w = load_data()
+    #w = load_data()
 
     route("/jsonpayload", method = POST) do
-        #@show jsonpayload()
-        #@show rawpayload()
-
         try
-            user_input = get_user_input(String(rawpayload()))
-            result = calculate(w, user_input)        
-            json(result)
+            #user_input = get_user_input(String(rawpayload()))
+            #result = calculate(w, user_input)        
+            #json(result)
+            json(Dict("success" => ";-"))
         catch e
             json(Dict("error" => String(e)))
         end        
