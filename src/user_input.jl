41ea7c0d7fce84ec62ea3be9a2761d40325f5188
codelrond78 <codelrond@protonmail.com>
2022-05-20 09:07:32 +0000

    all checked inputs

--- src/user_input.jl
+++ src/user_input.jl
@@ -18,6 +18,25 @@ struct UserInput
    typeyearconsume
    numpanels
    bonopercentage
+   usecsv
+   ree
+   rented
+   socialb
+
+   power
+   powerrentedvalle
+   fixedrate
+   fixedvalle
+   vallerate
+   vallellano
+   picorate
+   otherconcepts
+   panelpower
+   batterycapacity
+   inversorpower
+   carannualkm
+   electriccarpower
+   carefficiency
 end
 
 StructTypes.StructType(::Type{UserInput}) = StructTypes.Struct()
@@ -33,48 +52,147 @@ function validate(input::UserInput)
 
     if isnothing(input.deep)
         push!(response, "deep: missing")
-    elseif input.deep < 0.0 
+    elseif !isa(input.deep, Number) || input.deep < 0.0 
         push!(response, "deep: must be 0.0 < deep")
     end
 
     if isnothing(input.width)
         push!(response, "width: missing")
-    elseif input.width < 0.0
+    elseif !isa(input.width, Number) || input.width < 0.0
         push!(response, "width: must be 0.0 < width")
     end
 
     if isnothing(input.yearconsume)
         push!(response, "yearconsume: missing")
-    elseif input.yearconsume < 0
+    elseif !isa(input.yearconsume, Number) || input.yearconsume < 0
         push!(response, "yearconsume: must be yearconsume >= 0")
     end
 
     if isnothing(input.typedayconsume)
         push!(response, "typedayconsume: missing")
-    elseif !(1 <= input.typedayconsume <= 3)
+    elseif !isinteger(input.typedayconsume) || !(1 <= input.typedayconsume <= 3)
         push!(response, "typedayconsume: must be typedayconsume in [1, 2, 3]")
     end
 
     if isnothing(input.typeyearconsume)
         push!(response, "typeyearconsume: missing")
-    elseif !(1 <= input.typeyearconsume <= 3)
+    elseif !isinteger(input.typeyearconsume) || !(1 <= input.typeyearconsume <= 3)
         push!(response, "typeyearconsume: must be typeyearconsume in [1, 2, 3]")
     end
 
     if isnothing(input.numpanels)
         push!(response, "numpanels: missing")
-    elseif input.numpanels < 0
+    elseif !isinteger(input.numpanels) || input.numpanels < 0
         push!(response, "numpanels: must be numpanels >= 0")
     end
 
     if isnothing(input.bonopercentage)
         push!(response, "bonopercentage: missing")
-    elseif !(0 <= input.bonopercentage <= 1)
+    elseif !isinteger(input.bonopercentage) || !(0 <= input.bonopercentage <= 1)
         push!(response, "bonopercentage: must be bonopercentage in [0, 1]")
     end
 
+    if isnothing(input.usecsv)
+        push!(response, "usecsv: missing")
+    end
+
+    if isnothing(input.ree)
+        push!(response, "ree: missing")
+    end
+
+    if isnothing(input.rented)
+        push!(response, "rented: missing")
+    end
+
+    if isnothing(input.socialb)
+        push!(response, "socialb: missing")
+    end
+
+    if isnothing(input.power )
+        push!(response, "power: missing")
+    elseif !isa(input.power , Number) || input.power < 0
+        push!(response, "power: must be power >= 0")
+    end
+
+    if isnothing(input.powerrentedvalle )
+        push!(response, "powerrentedvalle: missing")
+    elseif !isa(input.powerrentedvalle , Number) || input.powerrentedvalle < 0
+        push!(response, "powerrentedvalle: must be powerrentedvalle >= 0")
+    end
+
+    if isnothing(input.fixedrate )
+        push!(response, "fixedrate: missing")
+    elseif !isa(input.fixedrate , Number) || input.fixedrate < 0
+        push!(response, "fixedrate: must be fixedrate >= 0")
+    end
+
+    if isnothing(input.fixedvalle )
+        push!(response, "fixedvalle: missing")
+    elseif !isa(input.fixedvalle , Number) || input.fixedvalle < 0
+        push!(response, "fixedvalle: must be fixedvalle >= 0")
+    end
+
+    if isnothing(input.vallerate )
+        push!(response, "vallerate: missing")
+    elseif !isa(input.vallerate , Number) || input.vallerate < 0
+        push!(response, "vallerate: must be vallerate >= 0")
+    end
+
+    if isnothing(input.vallellano )
+        push!(response, "vallellano: missing")
+    elseif !isa(input.vallellano , Number) || input.vallellano < 0
+        push!(response, "vallellano: must be vallellano >= 0")
+    end
+
+    if isnothing(input.picorate )
+        push!(response, "picorate: missing")
+    elseif !isa(input.picorate , Number) || input.picorate < 0
+        push!(response, "picorate: must be picorate >= 0")
+    end
+
+    if isnothing(input.otherconcepts )
+        push!(response, "otherconcepts: missing")
+    elseif !isa(input.otherconcepts , Number) || input.otherconcepts < 0
+        push!(response, "otherconcepts: must be otherconcepts >= 0")
+    end
+
+    if isnothing(input.panelpower )
+        push!(response, "panelpower: missing")
+    elseif !isa(input.panelpower , Number) || input.panelpower < 0
+        push!(response, "panelpower: must be panelpower >= 0")
+    end
+
+    if isnothing(input.batterycapacity )
+        push!(response, "batterycapacity: missing")
+    elseif !isa(input.batterycapacity , Number) || input.batterycapacity < 0
+        push!(response, "batterycapacity: must be batterycapacity >= 0")
+    end
+
+    if isnothing(input.inversorpower )
+        push!(response, "inversorpower: missing")
+    elseif !isa(input.inversorpower , Number) || input.inversorpower < 0
+        push!(response, "inversorpower: must be inversorpower >= 0")
+    end
+
+    if isnothing(input.carannualkm )
+        push!(response, "carannualkm: missing")
+    elseif !isa(input.carannualkm , Number) || input.carannualkm < 0
+        push!(response, "carannualkm: must be carannualkm >= 0")
+    end
+
+    if isnothing(input.electriccarpower )
+        push!(response, "electriccarpower: missing")
+    elseif !isa(input.electriccarpower , Number) || input.electriccarpower < 0
+        push!(response, "electriccarpower: must be electriccarpower >= 0")
+    end
+
+    if isnothing(input.carefficiency )
+        push!(response, "carefficiency: missing")
+    elseif !isa(input.carefficiency , Number) || input.carefficiency < 0
+        push!(response, "carefficiency: must be carefficiency >= 0")
+    end
+
     if length(response) != 0 
-        #throw(InputErrors(join(response, "\n")))
         throw(InputErrors(response))
     end
 end
